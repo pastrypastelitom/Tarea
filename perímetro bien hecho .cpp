f15cdd9239c37f30056ba88c87d3d1d8f8cacd86
ines <eduartillan@gmail.com>
2025-04-26 20:52:29 -0300

    ahora SI perimetro bien hecho
    perimetro bien hecho. profe perdon si esta todo hecho a los apurones pero me duele la espalda y los ojos.

---
+++ perímetro bien hecho .cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() 
+{ 
+//este programa calculara el valor del perimetro del cuadrado
+int n1 ;
+int n2=4 ;
+cout << "ingrese el valor del lado del cuadrado" << endl;
+cin >>n1 ;
+int perimetro ;
+perimetro= n1*n2 ;
+cout << "el perimetro es"<<endl<<perimetro ; //hace la multiplicación y te da el resultado del perimetro del cuadrado
+ }
